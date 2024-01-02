38d15f6bdb3bbf3fd2b41f3eba7999205a0f2708
AliWar98 <154895774+AliWar98@users.noreply.github.com>
2024-01-02 14:41:01 +0100

    Update CPUlimit.py

--- CPUlimit.py
+++ CPUlimit.py
@@ -6,20 +6,24 @@ from mininet.cli import CLI
 from mininet.node import CPULimitedHost
 
 # Skapa ett Mininet-objekt med CPULimitedHost som standardhost
-net = Mininet(host_CPULimitedHost)
+net = Mininet(host=CPULimitedHost)
 
 # Skapa host, switch och controller
-h1 = net.addHost('h1',cpu=.1)
-h2 = net.addHost('h2',cpu=.1)
-h3 = net.addHost('h3',cpu=.1)
+h1 = net.addHost('h1',cpu=1.0)
+h2 = net.addHost('h2',cpu=1.0)
+h3 = net.addHost('h3',cpu=1.0)
 s1 = net.addSwitch('s1')
 c0 = net.addController('c0', ip='127.0.0.1', port=6653) #Skapa en controller 'c0' med IP '127.0.0.1' och port '6653'
 
 # Lägg till länkar mellan host och switch med konfigurerade länkalternativ
-net.addLink(h1, s1, cls=TCLink, **link_opts)
-net.addLink(h2, s1, cls=TCLink, **link_opts)
-net.addLink(h3, s1, cls=TCLink, **link_opts)
+net.addLink(h1, s1)
+net.addLink(h2, s1)
+net.addLink(h3, s1)
 
+
+# Apply cpulimit to limit all processes on each host
+for host in net.hosts:
+     host.cmd('cpulimit --limit=10 --background')
 # Starta nätverket
 net.start()
 
@@ -28,3 +32,4 @@ CLI(net)
 
 # Stoppa nätverket efter att CLI-sessionen är avslutad
 net.stop()
+
